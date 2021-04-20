c411076e26998ad2916e7242c78d70628dc250fd
Felipe-CdS <felipe32santos@hotmail.com>
2021-04-20 13:38:40 -0300

    bot update

--- bot.py
+++ bot.py
@@ -11,5 +11,11 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+imagePath = "img.jpg";
+tweetString = dt.datetime.now().strftime("%x %X");
+tweetString += "\nHora de coletar pontos no TheKKing, Once!";
 
-api.update_status("Vai votar!")
+if(dt.datetime.now().hour == 13):
+    tweetString += "\nNão se esqueça do Mubeat também, já passou 24h!";
+
+api.update_with_media(imagePath, tweetString);
