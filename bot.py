590a53067deb8969b2ed4b5fb89bb969b56b364a
Felipe-CdS <felipe32santos@hotmail.com>
2021-04-20 14:14:09 -0300

    bot update

--- bot.py
+++ bot.py
@@ -2,17 +2,26 @@ import tweepy, time, sys
 from os import environ
 import datetime as dt
 
+## Consts
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
+imagePath = "img.jpg";
 
+## API Auth
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-imagePath = "img.jpg";
-tweetString = dt.datetime.now().strftime("%d/%m/%Y %H:%M");
+###Main Function
+hours = dt.datetime.now().hour;
+minutes = dt.datetime.now().minute;
+if(minutes < 10):
+    minutes = "0" + str(minutes);
+
+tweetString = dt.datetime.now().strftime("%d/%m/%Y ");
+tweetString += str(hours-3) + ":" + str(minutes);
 tweetString += "\nHora de coletar pontos no TheKKing, Once!";
 
 if(dt.datetime.now().hour == 13):
