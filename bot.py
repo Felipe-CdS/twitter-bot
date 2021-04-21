d8be38aab3d8ae03af0df8fbda5ad877a970fe9e
Felipe-CdS <felipe32santos@hotmail.com>
2021-04-20 21:55:00 -0300

    bot update

--- bot.py
+++ bot.py
@@ -17,10 +17,19 @@ api = tweepy.API(auth)
 ###Main Function
 hours = dt.datetime.now().hour;
 minutes = dt.datetime.now().minute;
+
+if(hours >= 0 and hours < 4):
+    hours = hours+21;
+    if(hours ==  24):
+        hours = 0;
+else:
+    hours -= 3;
+        
 if(minutes < 10):
     minutes = "0" + str(minutes);
-if(hours <= 0):
-    hours = 24 - hours;
+    
+if(hours < 10):
+    hours = "0" + str(hours);
 
 tweetString = dt.datetime.now().strftime("%d/%m/%Y ");
 tweetString += str(hours-3) + ":" + str(minutes);
